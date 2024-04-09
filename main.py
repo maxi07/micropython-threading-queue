193ed3ecbcd6fc9052a5e3dd6d17ec750239a344
Maximilian Krause <maxi.krause@me.com>
2024-04-09 22:41:31 +0200

    fix animation

--- main.py
+++ main.py
@@ -32,10 +32,11 @@ async def ui_task():
             continue
 
         old_val, new_val = msg
-        print(f"Setting LEDs from {old_val} to {new_val}")
+        print(f"Setting visible LEDs from {old_val} to {new_val}")
         if new_val - old_val > 0:
             for j in range(old_val, new_val):
-                if j > 0:
+                if j >= 0:
+                    print(f"a) Turning on LED with index {j}")
                     brightness = 0
                     while brightness <= 100:
                         np[j] = (0, brightness, 0)
@@ -43,9 +44,10 @@ async def ui_task():
                         np.write()
                     await asyncio.sleep(0.01)
                 else:
+                    print(f"b) Turning off LED with index {j - 1}")
                     brightness = 100
                     while brightness >= 0:
-                        np[j*(-1)] = (brightness, 0, 0)
+                        np[j*(-1) - 1] = (brightness, 0, 0)
                         brightness -= 1
                         np.write()
                     await asyncio.sleep(0.01)
@@ -54,15 +56,17 @@ async def ui_task():
             for j in reversed(range(new_val, old_val)):
                 if j >= 0:
                     brightness = 100
+                    print(f"c) Turning off LED with index {j}")
                     while brightness >= 0:
                         np[j] = (0, brightness, 0)
                         brightness -= 1
                         np.write()
                     await asyncio.sleep(0.01)
                 else:
+                    print(f"d) Turning on LED with index {j - 1}")
                     brightness = 0
                     while brightness <= 100:
-                        np[j*(-1)] = (brightness, 0, 0)
+                        np[j*(-1) - 1] = (brightness, 0, 0)
                         brightness += 1
                         np.write()
                     await asyncio.sleep(0.01)
@@ -72,18 +76,34 @@ async def ui_task():
 async def backend_task():
     old_val = 0
     new_val = 0
+
+    # backend_to_ui_queue.put((0, 5))
+    # await asyncio.sleep(3)
+    # backend_to_ui_queue.put((5, 7))
+    # await asyncio.sleep(3)
+    # backend_to_ui_queue.put((7, 3))
+    # await asyncio.sleep(3)
+    # backend_to_ui_queue.put((3, 0))
+    # await asyncio.sleep(3)
+    # backend_to_ui_queue.put((0, -3))
+    # await asyncio.sleep(3)
+    # backend_to_ui_queue.put((-3, 3))
+    # await asyncio.sleep(3)
+    # exit()
+
     while True:
         while new_val == old_val:
-            new_val = random.randint(((-1) * num_leds) + 1, num_leds - 1)
+            new_val = random.randint(((-1) * test_range), test_range)
             # new_val = random.randint(1, num_leds + 1)
 
         print(f"New LED count: {new_val}")
         backend_to_ui_queue.put((old_val, new_val))
         old_val = new_val
-        await asyncio.sleep(2)
+        await asyncio.sleep(5)
 
 # Initialisierung Neopixel
-num_leds = 36
+num_leds = 64
+test_range = 64
 np = NeoPixel(Pin(28, Pin.OUT), num_leds)
 
 
